5b421ce55fc1b61e263ec431a28302f8d71b4e5f
csnanx <210231094+csnanx@users.noreply.github.com>
2025-05-26 22:14:13 +0530

    Add game logic to a separate function

--- main.py
+++ main.py
@@ -14,25 +14,26 @@ def check_difficulty(difficulty):
     elif difficulty == "hard":
         return 5
 
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
+def guess_the_number():
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
 
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
-chances = check_difficulty(difficulty)
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    chances = check_difficulty(difficulty)
 
-computer_choice = random.randint(1, 100)
+    computer_choice = random.randint(1, 100)
 
-while chances:
-    print(f"You have {chances} attempts remaining to guess the number.")
-    guess = int(input("Make a guess: "))
+    while chances:
+        print(f"You have {chances} attempts remaining to guess the number.")
+        guess = int(input("Make a guess: "))
 
-    is_right = check_guess(computer_choice, guess)
-    if is_right:
-        chances = 0
-        print("Congrats!\nYou are absolutely RIGHT.")
-    else:
-        chances -= 1
-        if chances != 0:
-            print("Guess again")
+        is_right = check_guess(computer_choice, guess)
+        if is_right:
+            chances = 0
+            print("Congrats!\nYou are absolutely RIGHT.")
+        else:
+            chances -= 1
+            if chances != 0:
+                print("Guess again")
 
-print(f"The right answer was: {computer_choice}")
+    print(f"The right answer was: {computer_choice}")
