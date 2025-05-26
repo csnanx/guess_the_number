19814f4cdddbbcc8e150e820de79602c4843d49d
csnanx <210231094+csnanx@users.noreply.github.com>
2025-05-26 22:00:41 +0530

    Remove showing computer's choice in the screen

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
 chances = check_difficulty(difficulty)
 
 computer_choice = random.randint(1, 100)
-print(computer_choice)
+
 while chances:
     print(f"You have {chances} attempts remaining to guess the number.")
     guess = int(input("Make a guess: "))
